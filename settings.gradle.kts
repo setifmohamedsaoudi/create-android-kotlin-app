76bf2d3f0ddf49cf31429fdf0503bc76e91aa28b
setifmohamedsaoudi <setifmohamedsaoudi@gmail.com>
2025-11-23 14:25:09 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,19 @@
-include(":app", ":backend")
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MedExpert"
+include(":app")
+// إزالة ":base" من الإدراج لأنه غير موجود
