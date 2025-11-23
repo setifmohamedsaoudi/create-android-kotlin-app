ec85b04feab47acc1139f8772b7610e37f9ed3d6
setifmohamedsaoudi <setifmohamedsaoudi@gmail.com>
2025-11-23 14:25:36 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,22 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
         google()
-        jcenter()
+        mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.2")
-        classpath(kotlin("gradle-plugin", version = "1.4.30"))
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.3")
-        classpath("com.akaita.android:easylauncher:1.3.1")
+        classpath("com.android.tools.build:gradle:8.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
     }
 }
 
 allprojects {
     repositories {
         google()
-        maven { url = uri("https://jitpack.io") }
-        jcenter()
+        mavenCentral()
     }
 }
 
-tasks.register("clean", Delete::class.java, Action<Delete> {
+tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
-})
+}
